56fe403b84cb9169375d28ebb7bf0dd896da1e70
pmac8 <29241044+pmac8@users.noreply.github.com>
2024-04-09 13:14:15 -0400

    Started project over to see if I could make it flow better

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
-const container = document.querySelector('.container');
+const sizeOfGrid = 16
+const container = document.querySelector('.container')
 
-// Add funtion to create a grid
-function createBoxes(numberPerRow) {
-    
-    const total = (numberPerRow * numberPerRow) + numberPerRow;
-    const mod = numberPerRow + 1;
-  
-    for (let i = 1; i < total; i++) {
-      const div = document.createElement('div');
-  
-      if (i % mod === 0) {
-        div.style.cssText = "border: 0; height: 0; width: 100%";
-      } else {
-        div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
-      }
-      // Colors background when mouse enters a square in the grid
-        div.addEventListener('mouseenter', () => {
-          div.style.backgroundColor = 'blue'
-        })
-      container.appendChild(div);
+const createGrid = (amtOfGrids) => {
+  for (let i = 0; i < amtOfGrids; i++) {
+    const row = document.createElement('div')
+    row.classList.add('grid-row')
+
+    for (let j = 0; j < amtOfGrids; j++) {
+      const gridBox = document.createElement('div')
+      gridBox.classList.add('grid-box')
+      gridBox.addEventListener('mouseenter', () => {
+        gridBox.style.backgroundColor = 'black'
+      })
+
+      row.appendChild(gridBox)
     }
+    container.appendChild(row)
   }
+}
+createGrid(sizeOfGrid)
   
-  createBoxes(16);
 
-  
     
   
   
